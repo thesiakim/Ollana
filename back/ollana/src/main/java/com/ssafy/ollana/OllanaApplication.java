3afa5b2ca8b2ff1c0271d9d1f9584b4042e52c56
박찬미 <pcm0902@naver.com>
2025-04-24 14:21:36 +0900

    [박찬미] #Docs : 백 프로젝트 생성

---
+++ back/ollana/src/main/java/com/ssafy/ollana/OllanaApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.ollana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OllanaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OllanaApplication.class, args);
+    }
+
+}
