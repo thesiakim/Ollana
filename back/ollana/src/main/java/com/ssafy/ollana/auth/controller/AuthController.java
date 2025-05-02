beaa6e19d168c82b677f6a5b087cc3fef64d9d78
박찬미 <pcm0902@naver.com>
2025-05-02 11:45:31 +0900

    [박찬미] #Feat Auth : Back 회원가입 이미지 업로드 기능 구현 완료

--- back/ollana/src/main/java/com/ssafy/ollana/auth/controller/AuthController.java
+++ back/ollana/src/main/java/com/ssafy/ollana/auth/controller/AuthController.java
@@ -10,10 +10,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/auth")
@@ -23,8 +21,10 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/signup")
-    public ResponseEntity<Response<Void>> signup(@RequestBody SignupRequestDto request) {
-        authService.signup(request);
+    public ResponseEntity<Response<Void>> signup(
+            @RequestPart("userData") SignupRequestDto request,
+            @RequestPart(value = "profileImage", required = false) MultipartFile profileImage) {
+        authService.signup(request, profileImage);
         return ResponseEntity.ok(Response.success());
     }
 
