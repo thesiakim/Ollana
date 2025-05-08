c38039e9de1488e20fee633773921e1a361c5b74
신우진 <zebra0345@naver.com>
2025-05-08 14:49:01 +0900

    [신우진] #Feat: AI 유저 ID 받아서 추천 반환

--- ai/main.py
+++ ai/main.py
@@ -110,4 +110,4 @@ async def recommend(user_id:str, db: Session = Depends(get_db)):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
