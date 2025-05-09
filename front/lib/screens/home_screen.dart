cb442b7fd699ca1325b77753e9e46c38af99c205
thesiakim <siakim.1994@gmail.com>
2025-05-09 17:51:01 +0900

    [김시아] #Feat : 발자취 조회 페이지 구현 중

--- front/lib/screens/home_screen.dart
+++ front/lib/screens/home_screen.dart
@@ -10,7 +10,7 @@ import '../widgets/custom_app_bar.dart';
 import '../widgets/custom_footer.dart';
 import '../widgets/home_body.dart';
 import './tracking/tracking_screen.dart';
-import '../screens/user/my_page_screen.dart';
+import './footprint/my_footprint_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -20,7 +20,7 @@ class HomeScreen extends StatelessWidget {
     return Consumer<AppState>(
       builder: (context, appState, child) {
         return Scaffold(
-          appBar: const CustomAppBar(),
+          appBar: const CustomAppBar(), 
           body: Stack(
             children: [
               // 현재 선택된 탭에 따라 화면 표시
@@ -33,9 +33,9 @@ class HomeScreen extends StatelessWidget {
                   case 2:
                     return const Center(child: Text('산 정보 페이지'));
                   case 3:
-                    return const Center(child: Text('나의 발자취 페이지'));
+                    return const MyFootprintScreen(); 
                   case 4:
-                    return const MyPageScreen();
+                    return const Center(child: Text('마이페이지'));
                   default:
                     return HomeBody();
                 }
