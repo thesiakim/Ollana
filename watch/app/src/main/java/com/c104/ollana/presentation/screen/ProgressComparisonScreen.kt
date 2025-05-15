593047938d4d02a68de971c7d701ea94e8442396
huiju <gmlwn6316@gmail.com>
2025-05-16 01:45:20 +0900

    [장희주] #Fix Watch : 실시간 과거의 데이터 비교 UI 알림 백그라운드에서도 진행으로 수정

---
+++ watch/app/src/main/java/com/c104/ollana/presentation/screen/ProgressComparisonScreen.kt
@@ -0,0 +1,65 @@
+package com.c104.ollana.presentation.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.wear.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.c104.ollana.R
+
+//실시간 비교 결과를 표시하는 화면
+@Composable
+fun ProgressComparisonScreen(progressMessage : String){
+    val isFast = progressMessage.contains("🐇")
+    val isSlow = progressMessage.contains("🐢")
+
+    val distanceInfo = progressMessage.substringAfter(" ").trim()
+
+    Box(
+        modifier=Modifier
+            .fillMaxSize()
+            .background(Color.Black),
+        contentAlignment  = Alignment.Center
+    ){
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ){
+            if(isFast){
+                Image(
+                    painter= painterResource(id=R.drawable.rabbit),
+                    contentDescription = "토끼",
+                    modifier = Modifier.size(100.dp),
+                    contentScale = ContentScale.Fit
+                )
+            }else if(isSlow){
+                Image(
+                    painter = painterResource(id = R.drawable.turtle), // 🐢 이미지
+                    contentDescription = "거북이",
+                    modifier = Modifier.size(100.dp),
+                    contentScale = ContentScale.Fit
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text=distanceInfo,
+                fontSize = 20.sp,
+                color=Color.White,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+
+    }
+
+}
+
