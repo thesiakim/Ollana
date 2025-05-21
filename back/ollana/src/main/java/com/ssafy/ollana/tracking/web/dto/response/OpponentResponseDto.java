3daef342de9e2bf46977333c7db8b60e0b919b60
thesiakim <siakim.1994@gmail.com>
2025-05-21 21:36:16 +0900

    [김시아] #Feat : 트래킹 시작 API 호출 시 상대의 평균 심박수, 최대 심박수 추가하여 응답

--- back/ollana/src/main/java/com/ssafy/ollana/tracking/web/dto/response/OpponentResponseDto.java
+++ back/ollana/src/main/java/com/ssafy/ollana/tracking/web/dto/response/OpponentResponseDto.java
@@ -1,5 +1,6 @@
 package com.ssafy.ollana.tracking.web.dto.response;
 
+import com.ssafy.ollana.footprint.persistent.entity.HikingHistory;
 import com.ssafy.ollana.tracking.persistent.entity.HikingLiveRecords;
 import com.ssafy.ollana.user.entity.User;
 import lombok.Builder;
@@ -12,12 +13,16 @@ import java.util.List;
 public class OpponentResponseDto {
     private Integer opponentId;
     private String nickname;
+    private Integer maxHeartRate;
+    private Double averageHeartRate;
     private List<BattleRecordsForTrackingResponseDto> records;
 
-    public static OpponentResponseDto from(User opponent, List<HikingLiveRecords> records) {
+    public static OpponentResponseDto from(User opponent, HikingHistory hikingHistory, List<HikingLiveRecords> records) {
         return OpponentResponseDto.builder()
                 .opponentId(opponent.getId())
                 .nickname(opponent.getNickname())
+                .averageHeartRate(hikingHistory != null ? hikingHistory.getAverageHeartRate() : null)
+                .maxHeartRate(hikingHistory != null ? hikingHistory.getMaxHeartRate() : null)
                 .records(records.stream()
                         .map(BattleRecordsForTrackingResponseDto::from)
                         .toList())
